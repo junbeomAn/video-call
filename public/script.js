b855515dbb824cfac8da22505bc702b0bec5adef
junbeoman <wnsqja8744@gmail.com>
2020-12-17 21:44:42 +0900

    video-call application

---
+++ public/script.js
@@ -0,0 +1,61 @@
+const socket = io("/");
+const videoGrid = document.getElementById("video-grid");
+const myPeer = new Peer(undefined, {
+  host: "/",
+  port: 3001,
+});
+const myVideo = document.createElement("video");
+myVideo.muted = true;
+const peers = {};
+
+navigator.mediaDevices
+  .getUserMedia({
+    video: true,
+    audio: true,
+  })
+  .then((stream) => {
+    addVideoStream(myVideo, stream);
+
+    myPeer.on("call", (call) => {
+      // receive calls
+      call.answer(stream);
+      call.on("stream", (remoteStream) => {
+        const video = document.createElement("video");
+        addVideoStream(video, remoteStream);
+      });
+    });
+
+    socket.on("user-connected", (userId) => {
+      connectToNewUser(userId, stream);
+    });
+  });
+
+socket.on("user-disconnected", (userId) => {
+  peers[userId] && peers[userId].close();
+});
+
+function connectToNewUser(userId, stream) {
+  // make calls when new users connect to our room
+  const call = myPeer.call(userId, stream);
+  const video = document.createElement("video");
+  // video.muted = true;
+  call.on("stream", (remoteStream) => {
+    addVideoStream(video, remoteStream);
+  });
+  call.on("close", () => {
+    video.remove();
+  });
+  peers[userId] = call;
+}
+
+myPeer.on("open", (id) => {
+  socket.emit("join-room", ROOM_ID, id);
+});
+
+function addVideoStream(video, stream) {
+  video.srcObject = stream;
+  video.addEventListener("loadedmetadata", () => {
+    video.play();
+  });
+  videoGrid.append(video);
+}
---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+const { v4: uuidV4 } = require("uuid");
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.redirect(`/${uuidV4()}`);
+});
+
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.params.room });
+});
+
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId) => {
+    socket.join(roomId);
+    socket.to(roomId).broadcast.emit("user-connected", userId);
+
+    socket.on("disconnect", () => {
+      socket.to(roomId).broadcast.emit("user-disconnected", userId);
+    });
+  });
+});
+
+http.listen(3000);
